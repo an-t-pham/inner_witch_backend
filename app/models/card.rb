049ac48a298729451cf24e9407f6a70464dcbac5
anpham148 <anpham148@gmail.com>
2021-03-09 15:51:09 +0000

    added card random method

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,8 @@
 class Card < ApplicationRecord
     has_many :card_readings, dependent: :destroy
     has_many :readings, through: :card_readings
+
+    def self.random_all
+        self.all.map {|c| c.id}.shuffle
+    end
 end
